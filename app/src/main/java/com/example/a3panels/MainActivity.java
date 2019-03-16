45b07424ba1a9ae205f7a6e5bab04785406e9d09
Vixtor61 <00198117@uca.edu.sv>
2019-03-16 01:36:39 -0600

    finish

--- app/src/main/java/com/example/a3panels/MainActivity.java
+++ app/src/main/java/com/example/a3panels/MainActivity.java
@@ -10,15 +10,20 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int color;
+    ArrayList<Integer> r = new ArrayList<Integer>();
+    ArrayList<Integer> g = new ArrayList<Integer>();
+    ArrayList<Integer> b = new ArrayList<Integer>();
     //private LinearLayout mLinearLayout;
     private Button btn_1,btn_2,btn_3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main) ;
+        setContentView(R.layout.activity_main);
+        addColors();
         //mLinearLayout = findViewById(R.id.ll);
         btn_1 = findViewById(R.id.btn_1);
         btn_2 = findViewById(R.id.btn_2);
@@ -39,17 +44,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
                 Log.d("color","daskl");
-                btn_1.setBackgroundColor(Color.parseColor(retrurnNewColor(1)));
-
+                //btn_1.setBackgroundColor(Color.parseColor(retrurnNewColor(1)));
+                btn_1.setBackgroundColor(getResources().getColor(getColorFromList(1)));
                 //Toast.makeText(MainActivity.this,color,Toast.LENGTH_LONG).show();
 
                 break;
             case R.id.btn_2:
-                btn_2.setBackgroundColor(Color.parseColor(retrurnNewColor(2)));
+
+                btn_2.setBackgroundColor(getResources().getColor(getColorFromList(2)));
+                //btn_2.setBackgroundColor(Color.parseColor(retrurnNewColor(2)));
                 //Toast.makeText(MainActivity.this,"btn_2",Toast.LENGTH_LONG).show();
                 break;
             case R.id.btn_3:
-                btn_3.setBackgroundColor(Color.parseColor(retrurnNewColor(3)));
+                btn_3.setBackgroundColor(getResources().getColor(getColorFromList(3)));
+                //btn_3.setBackgroundColor(Color.parseColor(retrurnNewColor(3)));
                 //Toast.makeText(MainActivity.this,"btn_3",Toast.LENGTH_LONG).show();
                 break;
         }
@@ -79,6 +87,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         return "#ff00ff";
+    }
+    public int getColorFromList(int opt){
+        int result = 0;
+        int rand = (int) (Math.random()*6);
+        switch (opt){
+            case 1:
+                result = r.get(rand);
+                break;
+            case 2:
+                result = g.get(rand);
+                break;
+            case 3:
+                result = b.get(rand);
+                break;
+        }
+        return result;
+    }
+    public void addColors(){
+        r.add(R.color.color1);
+        r.add(R.color.color2);
+        r.add(R.color.color3);
+        r.add(R.color.color4);
+        r.add(R.color.color5);
+        r.add(R.color.color6);
+        g.add(R.color.color12);
+        g.add(R.color.color22);
+        g.add(R.color.color32);
+        g.add(R.color.color42);
+        g.add(R.color.color52);
+        g.add(R.color.color62);
+        b.add(R.color.color13);
+        b.add(R.color.color23);
+        b.add(R.color.color33);
+        b.add(R.color.color43);
+        b.add(R.color.color53);
+        b.add(R.color.color63);
     }
 
 }
